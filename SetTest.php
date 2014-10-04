95ea4e81035591bda0c4894bba524e62eb9f9c7a
Adrian Ko <adrianchko@gmail.com>
2014-10-04 16:02:05 +0100

    Add sorted set tests

--- SetTest.php
+++ SetTest.php
@@ -1,5 +1,6 @@
 <?php
 require_once("Set.php");
+require_once("SortedSet.php");
 
 function printSet($set, $action) {
     echo $action."\n";
@@ -22,3 +23,20 @@ $set->removeAll("d", "e", "f");
 printSet($set, "\$set->removeAll('d', 'e', 'f')");
 
 echo "\$set->size(): ".$set->size()."\n";
+
+$set2 = new SortedSet("a", "b", "c");
+printSet($set2, "Initialize, add: a, b, c");
+
+$set2->add("a");
+printSet($set2, "\$set->add('a')");
+
+$set2->retainAll("a", "c");
+printSet($set2, "\$set->retain('a', 'c')");
+
+$set2->addAll("d", "e", "f", "h", "g");
+printSet($set2, "\$set->addAll('d', 'e', 'f', 'h', 'g')");
+
+$set2->removeAll("d", "e", "f");
+printSet($set2, "\$set->removeAll('d', 'e', 'f')");
+
+echo "\$set->size(): ".$set2->size()."\n";
