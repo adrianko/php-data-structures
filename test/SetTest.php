43ae5918bfcc44a31038c904dd5e9c11e09b262d
Adrian Ko <adrianchko@gmail.com>
2014-10-05 00:35:24 +0100

    Add some docs

--- test/SetTest.php
+++ test/SetTest.php
@@ -2,26 +2,43 @@
 
 require_once(realpath(dirname(__FILE__))."/../src/Set.php");
 
+/**
+ * Class SetTest
+ *
+ * Unit tests for Set class
+ */
 class SetTest extends PHPUnit_Framework_TestCase {
 
+    /**
+     * @covers Set::__construct
+     */
     public function testSetInitialisation() {
         $exp = "Set";
         $act = new Set();
         $this->assertInstanceOf($exp, $act);
     }
 
+    /**
+     * @covers Set::toArray
+     */
     public function testSetToArray() {
         $exp = array("a", "b","c");
         $act = (new Set("a", "b", "c"))->toArray();
         $this->assertEquals($exp, $act);
     }
 
+    /**
+     * @covers Set::size
+     */
     public function testSetSize() {
         $exp = 5;
         $act = (new Set(1,2,3,4,5))->size();
         $this->assertEquals($exp, $act);
     }
 
+    /**
+     * @covers Set::add
+     */
     public function testSetAddTrue() {
         $exp = true;
         $set = new Set();
@@ -29,6 +46,9 @@ class SetTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($exp, $act);
     }
 
+    /**
+     * @covers Set::add
+     */
     public function testSetAddFalse() {
         $exp = false;
         $set = new Set();
@@ -37,6 +57,9 @@ class SetTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($exp, $act);
     }
 
+    /**
+     * @covers Set::remove
+     */
     public function testSetRemoveTrue() {
         $exp = true;
         $set = new Set("a", "b");
@@ -44,6 +67,9 @@ class SetTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($exp, $act);
     }
 
+    /**
+     * @covers Set::remove
+     */
     public function testSetRemoveFalse() {
         $exp = false;
         $set = new Set("a", "b");
@@ -51,6 +77,9 @@ class SetTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($exp, $act);
     }
 
+    /**
+     * @covers Set::remove
+     */
     public function testSetRemoveContents() {
         $exp = array("a");
         $set = new Set("a", "b");
@@ -59,6 +88,9 @@ class SetTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($exp, $act);
     }
 
+    /**
+     * @covers Set::isEmpty
+     */
     public function testSetIsEmptyTrue() {
         $exp = true;
         $set = new Set();
@@ -68,6 +100,9 @@ class SetTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($exp, $act);
     }
 
+    /**
+     * @covers Set::isEmpty
+     */
     public function testSetIsEmptyFalse() {
         $exp = false;
         $set = new Set();
