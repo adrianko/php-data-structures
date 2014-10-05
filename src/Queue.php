6e215579824f9b2041fa1eb5e0b65903a3d19f6d
Adrian Ko <adrianchko@gmail.com>
2014-10-05 19:16:45 +0100

    Queue class data structure

---
+++ src/Queue.php
@@ -0,0 +1,93 @@
+<?php
+
+require_once(realpath(dirname(__FILE__))."/../src/Collection.php");
+
+/**
+ * Queue Set - data type
+ *
+ * A queue data type implemented in PHP. Non synchronised. PHP however allows
+ * multi type arrays so haven't quite figured out the type situation yet.
+ *
+ * @author AK <akcodes3@gmail.com>
+ * @version 0.1
+ */
+class Queue extends Collection{
+
+    protected $elements;
+
+    /**
+     * Initializes the class and adds all args to the queue
+     */
+    public function __construct() {
+        $this->elements = array();
+        $args = func_get_args();
+        foreach($args as $a) {
+            $this->add($a);
+        }
+    }
+
+    /**
+     * @param $e
+     * @return bool
+     */
+    public function add($e) {
+        $this->elements[] = $e;
+
+        return true;
+    }
+
+    /**
+     * @param $e
+     * @return bool
+     */
+    public function offer($e) {
+        return $this->add($e);
+    }
+
+    /**
+     * @return mixed
+     * @throws Exception - no such element
+     */
+    public function remove() {
+        if(empty($this->elements)) {
+            throw new Exception("No such element");
+        }
+
+        return array_shift($this->elements);
+    }
+
+    /**
+     * @return mixed|null
+     */
+    public function poll() {
+        if(empty($this->elements)) {
+            return null;
+        }
+
+        return array_shift($this->elements);
+    }
+
+    /**
+     * @return mixed
+     * @throws Exception - no suh element
+     */
+    public function element() {
+        if(empty($this->elements)) {
+           throw new Exception("No such element");
+        }
+
+        return $this->elements[0];
+    }
+
+    /**
+     * @return mixed|null
+     */
+    public function peek() {
+        if(empty($this->elements)) {
+            return null;
+        }
+
+        return $this->elements[0];
+    }
+
+} 
